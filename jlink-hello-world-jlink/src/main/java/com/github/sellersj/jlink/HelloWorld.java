356c597a04cbb4969d90f51b6341df926ec35924
Jim Sellers <jim.sellers@gmail.com>
2018-06-27 20:38:47 -0400

    making it into a multimodule project

---
+++ jlink-hello-world-jlink/src/main/java/com/github/sellersj/jlink/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.github.sellersj.jlink;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world, java version is: " + System.getProperty("java.version"));
+  }
+
+}
