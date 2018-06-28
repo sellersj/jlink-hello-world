a19576a06d6cb3688b7e424ff32b11d30b6f2225
Jim Sellers <jim.sellers@gmail.com>
2018-06-27 21:33:47 -0400

    temp stab at it all

--- jlink-hello-world-jlink/src/main/java/com/github/sellersj/jlink/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.github.sellersj.jlink;
-
-public class HelloWorld {
-
-  public static void main(String[] args) {
-    System.out.println("Hello world, java version is: " + System.getProperty("java.version"));
-  }
-
-}
